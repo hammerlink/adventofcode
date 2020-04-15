199b0f4b3e4ca0e1d3f2ae3956c322709a4acf44
hendrik <hamerlinckhendrik@gmail.com>
2020-04-15 11:58:08 +0200

    day 1 exercise

---
+++ src/days/day_1.ts
@@ -0,0 +1,31 @@
+export namespace Day1 {
+    export function calculateRequiredFuel(mass: number): number {
+        return Math.floor(mass / 3) - 2;
+    }
+}
+
+if (!module.parent) {
+    const fs = require('fs');
+    const path = require('path');
+    const readline = require('readline');
+
+    async function main() {
+        const fileStream = fs.createReadStream(path.join(path.dirname(__filename), '../data/day_1.input'));
+
+        const rl = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity
+        });
+
+        let total = 0;
+        for await (const line of rl) {
+            if (!line) return;
+            total += Day1.calculateRequiredFuel(parseInt(line, 10));
+        }
+        return total;
+    }
+
+    main().then(result => {
+        console.log(result);
+    });
+}
