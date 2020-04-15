77c03070374fa17981e82054a54c3a6b70e4b18a
hendrik <hamerlinckhendrik@gmail.com>
2020-04-15 11:59:17 +0200

    day 1 clean code

--- src/days/day_1.ts
+++ src/days/day_1.ts
@@ -11,11 +11,7 @@ if (!module.parent) {
 
     async function main() {
         const fileStream = fs.createReadStream(path.join(path.dirname(__filename), '../data/day_1.input'));
-
-        const rl = readline.createInterface({
-            input: fileStream,
-            crlfDelay: Infinity
-        });
+        const rl = readline.createInterface({input: fileStream, crlfDelay: Infinity});
 
         let total = 0;
         for await (const line of rl) {
@@ -25,7 +21,5 @@ if (!module.parent) {
         return total;
     }
 
-    main().then(result => {
-        console.log(result);
-    });
+    main().then(console.log).catch(err => console.error(err));
 }
