0d6cf75f784f59d12a8e9f238edbef668a26d371
hendrik <hamerlinckhendrik@gmail.com>
2020-04-16 09:03:11 +0200

    day 6, part 2

--- src/days/day_6.spec.ts
+++ src/days/day_6.spec.ts
@@ -4,17 +4,26 @@ import {expect} from "chai";
 describe('advent of code, day 6', function () {
     it('should map the orbits', function () {
         const input = ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L'];
-        const orbitMap = Day6.parseRawInputMap(input);
-        expect(orbitMap?.B?.C?.D?.I).to.not.be.null;
-        expect(orbitMap?.B?.G?.H).to.not.be.null;
-        expect(orbitMap?.B?.C?.D?.E?.J?.K?.L).to.not.be.null;
-        expect(orbitMap?.B?.C?.D?.E?.F).to.not.be.null;
+        const orbitMap = Day6.parseRawInputMap(input).orbitMap;
+        expect(orbitMap.orbits?.B.orbits?.C.orbits?.D.orbits?.I).to.not.be.null;
+        expect(orbitMap.orbits?.B.orbits?.G.orbits?.H.orbits).to.not.be.null;
+        expect(orbitMap.orbits?.B.orbits?.C.orbits?.D.orbits?.E.orbits?.J.orbits?.K.orbits?.L).to.not.be.null;
+        expect(orbitMap.orbits?.B.orbits?.C.orbits?.D.orbits?.E.orbits?.F).to.not.be.null;
     });
 
     it('should calculate the orbit connections', function () {
         const input = ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L'];
-        const orbitMap = Day6.parseRawInputMap(input);
+        const orbitMap = Day6.parseRawInputMap(input).orbitMap;
         const totalConnections = Day6.calculateDistances(orbitMap, 0);
         expect(totalConnections).to.be.eq(42);
+    });
+
+    it('should get the closest transfer', function () {
+        const input = ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L', 'K)YOU', 'I)SAN'];
+        const parsedMap = Day6.parseRawInputMap(input);
+        const startPoint = parsedMap.voidPoint.orbits.YOU;
+        expect(startPoint).to.not.be.null;
+        const closestTarget = Day6.transferOrbit(startPoint, 'SAN');
+        expect(closestTarget).to.be.equal(4);
     });
 });
--- src/days/day_6.ts
+++ src/days/day_6.ts
@@ -1,32 +1,78 @@
 export namespace Day6 {
     export interface OrbitPoint {
-        comDistance?: any;
-
-        [orbitCode: string]: OrbitPoint;
+        orbitID: string;
+        comDistance?: number;
+        parentOrbit: OrbitPoint;
+        orbits: { [orbitCode: string]: OrbitPoint; };
     }
 
-    export function parseRawInputMap(lines: string[]): OrbitPoint {
-        const voidPoint: OrbitPoint = {};
+    export function parseRawInputMap(lines: string[]): { orbitMap: OrbitPoint, voidPoint: OrbitPoint } {
+        const voidPoint: OrbitPoint = {orbitID: null, orbits: {}, parentOrbit: null,};
         // register all points
         lines.forEach(line => {
             const orbits = line.split(')');
-            if (!voidPoint[orbits[0]]) voidPoint[orbits[0]] = {};
-            if (!voidPoint[orbits[1]]) voidPoint[orbits[1]] = {};
-            // lay orbit
-            voidPoint[orbits[0]][orbits[1]] = voidPoint[orbits[1]];
+            if (!voidPoint.orbits[orbits[0]]) voidPoint.orbits[orbits[0]] = {
+                orbitID: orbits[0],
+                parentOrbit: null,
+                orbits: {}
+            };
+            if (!voidPoint.orbits[orbits[1]]) voidPoint.orbits[orbits[1]] = {
+                orbitID: orbits[1],
+                parentOrbit: null,
+                orbits: {}
+            };
+            const parentOrbit = voidPoint.orbits[orbits[0]];
+            const childOrbit = voidPoint.orbits[orbits[1]];
+
+            parentOrbit.orbits[childOrbit.orbitID] = childOrbit;
+            childOrbit.parentOrbit = parentOrbit;
         });
-        return voidPoint.COM;
+        return {orbitMap: voidPoint.orbits.COM, voidPoint};
     }
 
     export function calculateDistances(orbit: OrbitPoint, currentDistance: number): number {
         let totalDistance = currentDistance;
-        for (const key in orbit) {
-            if (key === 'comDistance') continue;
-            const connectedOrbit: OrbitPoint = orbit[key];
+        for (const key in orbit.orbits) {
+            const connectedOrbit: OrbitPoint = orbit.orbits[key];
             connectedOrbit.comDistance = currentDistance + 1;
             totalDistance += calculateDistances(connectedOrbit, currentDistance + 1);
         }
         return totalDistance;
+    }
+
+    export function transferOrbit(currentOrbit: OrbitPoint, targetOrbitKey: string): number {
+        let closestTransfer: number = null;
+        let closestParentOrbit: OrbitPoint = null;
+
+        // start from parent orbit
+
+        function searchClosestOrbitPath(currentOrbit: OrbitPoint, currentSteps: number, orbitsPassed: string[]) {
+            if ((closestTransfer !== null && currentSteps > closestTransfer)
+                || orbitsPassed.find(x => x === currentOrbit.orbitID)) return;
+            if (!!currentOrbit.orbits[targetOrbitKey]) {
+                if (closestTransfer === null || currentSteps < closestTransfer) {
+                    closestTransfer = currentSteps;
+                    closestParentOrbit = currentOrbit;
+                }
+                return;
+            }
+            const newOrbitsPassed = [...orbitsPassed, currentOrbit.orbitID];
+            if (currentOrbit.parentOrbit)
+                searchClosestOrbitPath(currentOrbit.parentOrbit, currentSteps + 1, newOrbitsPassed);
+            for (const key in currentOrbit.orbits) {
+                searchClosestOrbitPath(currentOrbit.orbits[key], currentSteps + 1, newOrbitsPassed);
+            }
+        }
+
+        searchClosestOrbitPath(currentOrbit.parentOrbit, 0, [currentOrbit.orbitID]);
+
+        if (closestTransfer) {
+            const currentParent = currentOrbit.parentOrbit;
+            delete currentParent.orbits[currentOrbit.orbitID];
+            currentOrbit.parentOrbit = closestParentOrbit;
+            closestParentOrbit.orbits[currentOrbit.orbitID] = currentOrbit;
+        }
+        return closestTransfer;
     }
 }
 
@@ -45,10 +91,14 @@ if (!module.parent) {
             lines.push(line);
         }
 
-        const orbitMap = Day6.parseRawInputMap(lines);
+        const parsedOrbits = Day6.parseRawInputMap(lines);
+        const orbitMap = parsedOrbits.orbitMap;
         const totalConnections = Day6.calculateDistances(orbitMap, 0);
-        console.log(totalConnections);
+        console.log('part 1', totalConnections);
 
+        const startPoint = parsedOrbits.voidPoint.orbits.YOU;
+        const closestTarget = Day6.transferOrbit(startPoint, 'SAN');
+        console.log('part 2', closestTarget);
     }
 
     main().then(console.log).catch(err => console.error(err));
