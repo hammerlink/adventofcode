eddf9d00049a4314e5af9007fb94827c61a795f2
hendrik <hamerlinckhendrik@gmail.com>
2020-04-20 04:20:26 +0200

    day 11, part 2

--- src/days/day_11.ts
+++ src/days/day_11.ts
@@ -20,6 +20,8 @@ export namespace Day11 {
         currentDirection: Direction;
         currentX: number;
         currentY: number;
+        rangeX: [number, number];
+        rangeY: [number, number];
         differentPanelsPaintCount: number;
         spaceShip: SpaceShip;
     }
@@ -33,10 +35,15 @@ export namespace Day11 {
     }
 
     export function moveHullPainterInCurrentDirection(hullPainter: HullPainter) {
-        if (hullPainter.currentDirection === Direction.UP) return hullPainter.currentY++;
-        if (hullPainter.currentDirection === Direction.DOWN) return hullPainter.currentY--;
-        if (hullPainter.currentDirection === Direction.LEFT) return hullPainter.currentX--;
-        if (hullPainter.currentDirection === Direction.RIGHT) return hullPainter.currentX++;
+        if (hullPainter.currentDirection === Direction.UP) hullPainter.currentY++;
+        if (hullPainter.currentDirection === Direction.DOWN) hullPainter.currentY--;
+        if (hullPainter.currentDirection === Direction.LEFT) hullPainter.currentX--;
+        if (hullPainter.currentDirection === Direction.RIGHT) hullPainter.currentX++;
+
+        if (hullPainter.currentX < hullPainter.rangeX[0]) hullPainter.rangeX[0] = hullPainter.currentX;
+        if (hullPainter.currentX > hullPainter.rangeX[1]) hullPainter.rangeX[1] = hullPainter.currentX;
+        if (hullPainter.currentY > hullPainter.rangeY[1]) hullPainter.rangeY[1] = hullPainter.currentY;
+        if (hullPainter.currentY < hullPainter.rangeY[0]) hullPainter.rangeY[0] = hullPainter.currentY;
     }
 
     export function getInitialHullPainter(): HullPainter {
@@ -45,22 +52,29 @@ export namespace Day11 {
             currentX: 0,
             currentY: 0,
             differentPanelsPaintCount: 0,
-            spaceShip: {}
+            spaceShip: {},
+            rangeX: [0, 0],
+            rangeY: [0, 0],
         };
     }
 
-    export async function runHullPainterAreaTest(program: ProgramManager): Promise<number> {
-        const hullPainter = getInitialHullPainter();
+    export function getCurrentColor(hullPainter: HullPainter, x?: number, y?: number) {
+        if (!x && x !== 0) x = hullPainter.currentX;
+        if (!y && y !== 0) y = hullPainter.currentY;
+        if (!hullPainter.spaceShip[x]) hullPainter.spaceShip[x] = {};
+        if (!hullPainter.spaceShip[x][y])
+            hullPainter.spaceShip[x][y] = {
+                color: Color.BLACK,
+                paintedCount: 0
+            };
+        return hullPainter.spaceShip[x][y].color;
+    }
+
+    export async function runHullPainter(program: ProgramManager, hullPainter: HullPainter): Promise<HullPainter> {
         let inputAsked = false;
         program.getInput = async () => {
             inputAsked = true;
-            if (!hullPainter.spaceShip[hullPainter.currentX]) hullPainter.spaceShip[hullPainter.currentX] = {};
-            if (!hullPainter.spaceShip[hullPainter.currentX][hullPainter.currentY])
-                hullPainter.spaceShip[hullPainter.currentX][hullPainter.currentY] = {
-                    color: Color.BLACK,
-                    paintedCount: 0
-                };
-            return hullPainter.spaceShip[hullPainter.currentX][hullPainter.currentY].color;
+            return getCurrentColor(hullPainter);
         }
         program.writeOutput = output => {
             if (inputAsked) {
@@ -79,7 +93,24 @@ export namespace Day11 {
             moveHullPainterInCurrentDirection(hullPainter);
         };
         await program.executeProgram();
+        return hullPainter;
+    }
+
+    export async function runHullPainterAreaTest(program: ProgramManager): Promise<number> {
+        const hullPainter = getInitialHullPainter();
+        await runHullPainter(program, hullPainter);
         return hullPainter.differentPanelsPaintCount;
+    }
+
+    export function printHullPainterShip(hullPainter: HullPainter) {
+        for (let y = hullPainter.rangeY[1]; y >= hullPainter.rangeY[0]; y--) {
+            let line = '';
+            for (let x = hullPainter.rangeX[0]; x < hullPainter.rangeX[1]; x++) {
+                const panelColor = getCurrentColor(hullPainter, x, y);
+                line += ' ' + (panelColor === Color.BLACK ? '.' : 'O') + ' ';
+            }
+            console.log(line);
+        }
     }
 }
 
@@ -91,7 +122,14 @@ if (!module.parent) {
         const intCodes = ProgramEngine.compileProgram(lines[0]);
         const program = new ProgramManager(intCodes);
 
-        console.log('part 1', await Day11.runHullPainterAreaTest(program));
+        console.log('part 1', await Day11.runHullPainterAreaTest(program)); // 2021
+
+        const programPart2 = new ProgramManager(intCodes);
+        const hullPainter = Day11.getInitialHullPainter();
+        hullPainter.spaceShip[0] = {};
+        hullPainter.spaceShip[0][0] = {color: Day11.Color.WHITE, paintedCount: 0};
+        await Day11.runHullPainter(programPart2, hullPainter);
+        Day11.printHullPainterShip(hullPainter);
     }
 
     main().catch(err => console.error(err));
