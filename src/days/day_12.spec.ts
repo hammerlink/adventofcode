a3968ad88c024bafb30524fd8c67594a925c44cd
hendrik <hamerlinckhendrik@gmail.com>
2020-04-24 08:19:33 +0200

    day 12, part 2

--- src/days/day_12.spec.ts
+++ src/days/day_12.spec.ts
@@ -1,15 +1,21 @@
 import {Day12} from "./day_12";
 import {expect} from "chai";
 
+const moons: Day12.Moon[] = [
+    {id: 0, velocity: {x: 0, y: 0, z: 0}, position: {x: -1, y: 0, z: 2}},
+    {id: 1, velocity: {x: 0, y: 0, z: 0}, position: {x: 2, y: -10, z: -7}},
+    {id: 2, velocity: {x: 0, y: 0, z: 0}, position: {x: 4, y: -8, z: 8}},
+    {id: 2, velocity: {x: 0, y: 0, z: 0}, position: {x: 3, y: 5, z: -1}},
+];
+const moons2: Day12.Moon[] = [
+    {id: 0, velocity: {x: 0, y: 0, z: 0}, position: {x: -8, y: -10, z: 0}},
+    {id: 1, velocity: {x: 0, y: 0, z: 0}, position: {x: 5, y: 5, z: 10}},
+    {id: 2, velocity: {x: 0, y: 0, z: 0}, position: {x: 2, y: -7, z: 3}},
+    {id: 2, velocity: {x: 0, y: 0, z: 0}, position: {x: 9, y: -8, z: -3}},
+];
 describe('day 12', function () {
     describe('part 1', function () {
         it('should execute steps correctly', function () {
-            const moons: Day12.Moon[] = [
-                {id: 0, velocity: {x: 0, y: 0, z: 0}, position: {x: -1, y: 0, z: 2}},
-                {id: 1, velocity: {x: 0, y: 0, z: 0}, position: {x: 2, y: -10, z: -7}},
-                {id: 2, velocity: {x: 0, y: 0, z: 0}, position: {x: 4, y: -8, z: 8}},
-                {id: 2, velocity: {x: 0, y: 0, z: 0}, position: {x: 3, y: 5, z: -1}},
-            ];
 
             const moonsStep1 = Day12.simulateMoonMotions(moons, 1);
             expect(JSON.stringify(moonsStep1[0].position)).equal(JSON.stringify({x: 2, y: -1, z: 1}));
@@ -27,4 +33,30 @@ describe('day 12', function () {
             expect(JSON.stringify(moonsStep2[0].position)).equal(JSON.stringify({x: 5, y: -3, z: -1}));
         });
     })
+
+    describe('part 2', function () {
+        it('should calculate the least common multiple', function () {
+            expect(Day12.leastCommonMultiplier([18, 28, 44])).equals(2772);
+        });
+
+        it('should calculate the steps for one axis', function () {
+            expect(Day12.calculateStepsToRecurForAxis(moons.map(x => x.position.x))).equals(18);
+            expect(Day12.calculateStepsToRecurForAxis(moons.map(x => x.position.y))).equals(28);
+        });
+
+        it('should calculate the steps for one axis  with reverse', function () {
+            expect(Day12.calculateRecurTimeWithLeapsAndReverse(moons.map(x => x.position.x)).steps).equals(18);
+            expect(Day12.calculateRecurTimeWithLeapsAndReverse(moons.map(x => x.position.y)).steps).equals(28);
+        });
+
+        it('should calculate the steps for one axis 2', function () {
+            expect(Day12.calculateStepsForStartRecurForAxis(moons.map(x => x.position.x))).equals(18);
+            expect(Day12.calculateStepsForStartRecurForAxis(moons.map(x => x.position.y))).equals(28);
+        });
+
+        it('should calculate steps to recur for all moons', function () {
+            expect(Day12.calculateStepsToRecur(moons)).equals(2772);
+            expect(Day12.calculateStepsToRecur(moons2)).equals(4686774924);
+        });
+    });
 });
--- src/days/day_12.ts
+++ src/days/day_12.ts
@@ -58,6 +58,190 @@ export namespace Day12 {
         });
         return output;
     }
+
+    export function leastCommonMultiplier(input: number[]): number {
+        if (input.length < 2) throw new Error('lcm not enough numbers');
+        let currentLCM = lcm_two_numbers(input[0], input[1]);
+        for (let i = 2; i < input.length; i++) currentLCM = lcm_two_numbers(currentLCM, input[i]);
+        return currentLCM;
+    }
+
+    function lcm_two_numbers(x: number, y: number) {
+        return (!x || !y) ? 0 : Math.abs((x * y) / gcd_two_numbers(x, y));
+    }
+
+    function gcd_two_numbers(x, y) {
+        x = Math.abs(x);
+        y = Math.abs(y);
+        while (y) {
+            let t = y;
+            y = x % y;
+            x = t;
+        }
+        return x;
+    }
+
+    // didnt use
+    export function calculateRecurTimeWithLeapsAndReverse(axesPositions: number[]): { start: number, steps: number; } {
+        const SKIP_STEP = 1000;
+        let steps = 0;
+        const velocity = axesPositions.map(_ => 0);
+        const positions = axesPositions.map(x => x);
+        const history: Array<{ e: number; p: number }> = [];
+        let currentEnergy = getTotalEnergy(positions, velocity);
+        let currentIntPosition = getIntPrintPositions(positions);
+        history.push({e: currentEnergy, p: currentIntPosition});
+        while (true) {
+            executeTimeStepWithAxes(positions, velocity);
+            steps++;
+            currentEnergy = getTotalEnergy(positions, velocity);
+            currentIntPosition = getIntPrintPositions(positions);
+            history.push({e: currentEnergy, p: currentIntPosition});
+            // matching algorithm
+            if (steps % SKIP_STEP === 0) {
+                let matchingIndex: number = null;
+                // search for matches first
+                for (let i = steps - 1; i >= 0; i--) {
+                    const record = history[i];
+                    if (record.e !== currentEnergy || record.p !== currentIntPosition) continue;
+                    // match found, check reverse history & calculate first recurrence
+                    const delta = steps - i;
+                    for (let j = i; j >= 0; j--) {
+                        const firstRecord = history[j];
+                        const secondRecord = history[j + delta];
+                        if (firstRecord.e !== secondRecord.e || firstRecord.p !== secondRecord.p) {
+                            return {steps: delta, start: j + 1};
+                        }
+                    }
+                    return {steps: delta, start: 0};
+                }
+            }
+        }
+    }
+
+    export function executeTimeStepWithAxes(positions: number[], velocity: number[]) {
+        for (let i = 0; i < 4; i++) {
+            let currentPosition = positions[i];
+            for (let j = 0; j < 4; j++) {
+                if (i === j) continue;
+                if (positions[j] !== currentPosition) velocity[i] += positions[j] > currentPosition ? 1 : -1;
+            }
+        }
+        for (let i = 0; i < 4; i++) positions[i] += velocity[i];
+    }
+
+    export function getTotalEnergy(positions: number[], velocity: number[]): number {
+        return positions.reduce((total, currentValue, currentIndex, array) => {
+            return total + Math.abs(currentValue) * Math.abs(velocity[currentIndex]);
+        }, 0);
+    }
+
+    export function getIntPrintPositions(positions: number[]): number {
+        return positions[0] * 1000000 + positions[1] * 10000 + positions[2] * 100 + positions[3];
+    }
+
+    export function calculateStepsForStartRecurForAxis(axesPositions: number[]): number {
+        if (axesPositions.length !== 4) throw new Error('invalid axes input');
+        let steps = 0;
+        const velocity = axesPositions.map(_ => 0);
+        const positions = axesPositions.map(x => x);
+        let currentTotalEnergy = getTotalEnergy(positions, velocity);
+        const energyZeroHistory: Array<{ index: number, positions: string }> = [];
+
+        function execute1TimeStep() {
+            for (let i = 0; i < 4; i++) {
+                let currentPosition = positions[i];
+                for (let j = 0; j < 4; j++) {
+                    if (i === j) continue;
+                    if (positions[j] !== currentPosition) velocity[i] += positions[j] > currentPosition ? 1 : -1;
+                }
+            }
+            for (let i = 0; i < 4; i++) positions[i] += velocity[i];
+            if (currentTotalEnergy === 0) {
+                energyZeroHistory.push({index: steps, positions: JSON.stringify({p: positions, v: velocity})});
+            }
+            steps++;
+            currentTotalEnergy = getTotalEnergy(positions, velocity);
+        }
+
+        let recurring = false;
+        let currentLength = energyZeroHistory.length;
+        while (!recurring) {
+            execute1TimeStep();
+            if (currentLength < energyZeroHistory.length) {
+                currentLength = energyZeroHistory.length;
+                const current = energyZeroHistory[energyZeroHistory.length - 1].positions;
+                for (let i = 0; i < energyZeroHistory.length - 1; i++) {
+                    if (energyZeroHistory[i].positions === current) {
+                        console.log('found index', i);
+                        return steps - energyZeroHistory[i].index - 1;
+                    }
+                }
+            }
+
+            if (steps % 100000000 === 0) {
+                console.log(steps);
+            }
+        }
+    }
+
+    export function calculateStepsToRecurForAxis(axesPositions: number[]): number {
+        if (axesPositions.length !== 4) throw new Error('invalid axes input');
+        let steps = 0;
+        const velocity = axesPositions.map(_ => 0);
+        const positions = axesPositions.map(x => x);
+        let currentTotalEnergy = getTotalEnergy(positions, velocity);
+        const energyHistory = [];
+        const positionHistory = [];
+        let recurring = false;
+        while (!recurring) {
+            // calculate velocity
+            for (let i = 0; i < 4; i++) {
+                let currentPosition = positions[i];
+                for (let j = 0; j < 4; j++) {
+                    if (i === j) continue;
+                    if (positions[j] !== currentPosition) velocity[i] += positions[j] > currentPosition ? 1 : -1;
+                }
+            }
+            // apply velocity
+            for (let i = 0; i < 4; i++) positions[i] += velocity[i];
+            energyHistory.push(currentTotalEnergy); // previous history
+            positionHistory.push(getIntPrintPositions(positions));
+            let energyHistoryIndex = energyHistory.indexOf(currentTotalEnergy);
+            while (energyHistoryIndex !== steps && energyHistoryIndex !== -1 &&
+            positionHistory[steps] === positionHistory[energyHistoryIndex]) {
+                if (
+                    energyHistory[steps - 1] === energyHistory[energyHistoryIndex - 1] &&
+                    positionHistory[steps - 1] === positionHistory[energyHistoryIndex - 1] &&
+                    energyHistory[steps - 2] === energyHistory[energyHistoryIndex - 2] &&
+                    positionHistory[steps - 2] === positionHistory[energyHistoryIndex - 2]
+                ) { // check 3 steps back
+                    recurring = true;
+                    break;
+                }
+                if (recurring) break;
+                energyHistoryIndex = energyHistory.indexOf(currentTotalEnergy, energyHistoryIndex + 1);
+            }
+            if (recurring) break;
+            steps++;
+            if (steps % 1000000 === 0) {
+                console.log(steps);
+            }
+            currentTotalEnergy = getTotalEnergy(positions, velocity);
+        }
+        return steps - 2;
+    }
+
+    export function calculateStepsToRecur(moons: Moon[]): number {
+        const startMoons: Moon[] = JSON.parse(JSON.stringify(moons));
+        const recurringStepsX = calculateStepsForStartRecurForAxis(startMoons.map(x => x.position.x)); // 1666200
+        console.log(recurringStepsX);
+        const recurringStepsZ = calculateStepsForStartRecurForAxis(startMoons.map(x => x.position.z)); // 8533740
+        console.log(recurringStepsZ);
+        const recurringStepsY = calculateStepsForStartRecurForAxis(startMoons.map(x => x.position.y));
+        console.log(recurringStepsY);
+        return leastCommonMultiplier([recurringStepsX, recurringStepsY, recurringStepsZ]);
+    }
 }
 
 if (!module.parent) {
@@ -67,6 +251,6 @@ if (!module.parent) {
         {id: 2, velocity: {x: 0, y: 0, z: 0}, position: {x: 9, y: -16, z: -3}},
         {id: 2, velocity: {x: 0, y: 0, z: 0}, position: {x: 6, y: -1, z: 2}},
     ];
-    const resultMoons = Day12.simulateMoonMotions(moons, 1000);
-    console.log('part 1', Day12.calculateTotalEnergy(resultMoons));
+    console.log('part 1', Day12.calculateTotalEnergy(Day12.simulateMoonMotions(moons, 1000)));
+    console.log('part 2', Day12.calculateStepsToRecur(moons));
 }
