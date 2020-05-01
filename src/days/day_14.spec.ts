7507549407c81b57a4f2c4bdc2881ae53ca3375a
hammerlink <hamerlinckhendrik@gmail.com>
2020-05-01 14:13:15 +0200

    day 14, part 1 & 2

--- src/days/day_14.spec.ts
+++ src/days/day_14.spec.ts
@@ -2,6 +2,63 @@ import {Day14} from "./day_14";
 import {expect} from "chai";
 
 describe('day 14', function () {
+    const reactions0 = [
+        '9 ORE => 2 A',
+        '8 ORE => 3 B',
+        '7 ORE => 5 C',
+        '3 A, 4 B => 1 AB',
+        '5 B, 7 C => 1 BC',
+        '4 C, 1 A => 1 CA',
+        '2 AB, 3 BC, 4 CA => 1 FUEL',
+    ].map(Day14.parseChemicalReaction);
+
+    const reactions1 = [
+        '157 ORE => 5 NZVS',
+        '165 ORE => 6 DCFZ',
+        '44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL',
+        '12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ',
+        '179 ORE => 7 PSHF',
+        '177 ORE => 5 HKGWZ',
+        '7 DCFZ, 7 PSHF => 2 XJWVT',
+        '165 ORE => 2 GPVTF',
+        '3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT',
+    ].map(Day14.parseChemicalReaction);
+
+    const reactions2 = [
+        '2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG',
+        '17 NVRVD, 3 JNWZP => 8 VPVL',
+        '53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL',
+        '22 VJHF, 37 MNCFX => 5 FWMGM',
+        '139 ORE => 4 NVRVD',
+        '144 ORE => 7 JNWZP',
+        '5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC',
+        '5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV',
+        '145 ORE => 6 MNCFX',
+        '1 NVRVD => 8 CXFTF',
+        '1 VJHF, 6 MNCFX => 4 RFSQX',
+        '176 ORE => 6 VJHF',
+    ].map(Day14.parseChemicalReaction);
+
+    const reactions3 = [
+        '171 ORE => 8 CNZTR',
+        '7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL',
+        '114 ORE => 4 BHXH',
+        '14 VRPVC => 6 BMBT',
+        '6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL',
+        '6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT',
+        '15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW',
+        '13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW',
+        '5 BMBT => 4 WPTQ',
+        '189 ORE => 9 KTJDG',
+        '1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP',
+        '12 VRPVC, 27 CNZTR => 2 XDBXC',
+        '15 KTJDG, 12 BHXH => 5 XCVML',
+        '3 BHXH, 2 VRPVC => 7 MZWV',
+        '121 ORE => 7 VRPVC',
+        '7 XCVML => 6 RJRHP',
+        '5 BHXH, 4 VRPVC => 5 LTCX',
+    ].map(Day14.parseChemicalReaction);
+
     describe('part 1', function () {
         it('should parse the raw input correctly', function () {
             const reaction1: Day14.ChemicalReaction = {
@@ -17,34 +74,41 @@ describe('day 14', function () {
                 }));
         });
 
-        it('should calculate the least amount of ORE required for FUEL', function () {
-            const lines0 = [
-                '9 ORE => 2 A',
-                '8 ORE => 3 B',
-                '7 ORE => 5 C',
-                '3 A, 4 B => 1 AB',
-                '5 B, 7 C => 1 BC',
-                '4 C, 1 A => 1 CA',
-                '2 AB, 3 BC, 4 CA => 1 FUEL',
-            ];
-            const reactions0 = lines0.map(Day14.parseChemicalReaction);
-            expect(Day14.searchOptimalReactionCost('ORE', 'FUEL', 1, reactions0))
+        it('should pass example 0', function () {
+            expect(Day14.searchOptimalReactionCost('ORE', 'FUEL', 1,
+                reactions0)['ORE'].total)
                 .equals(165);
+        });
 
-            const lines = [
-                '157 ORE => 5 NZVS',
-                '165 ORE => 6 DCFZ',
-                '44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL',
-                '12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ',
-                '179 ORE => 7 PSHF',
-                '177 ORE => 5 HKGWZ',
-                '7 DCFZ, 7 PSHF => 2 XJWVT',
-                '165 ORE => 2 GPVTF',
-                '3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT',
-            ];
-            const reactions = lines.map(Day14.parseChemicalReaction);
-            expect(Day14.searchOptimalReactionCost('ORE', 'FUEL', 1, reactions))
+        it('should pass example 1', function () {
+            expect(Day14.searchOptimalReactionCost('ORE', 'FUEL', 1,
+                reactions1)['ORE'].total)
                 .equals(13312);
         });
+
+        it('should pass example 2', function () {
+            expect(Day14.searchOptimalReactionCost('ORE', 'FUEL', 1,
+                reactions2)['ORE'].total)
+                .equals(180697);
+        });
+
+        it('should pass example 3', function () {
+            expect(Day14.searchOptimalReactionCost('ORE', 'FUEL', 1,
+                reactions3)['ORE'].total)
+                .equals(2210736);
+        });
     });
+
+    describe('part 2', function () {
+        this.timeout(15000);
+        it('should pass example 3', function () {
+            expect(Day14.searchMaximalReactionOutput(reactions3)).equals(460664);
+        });
+        it('should pass example 1', function () {
+            expect(Day14.searchMaximalReactionOutput(reactions1)).equals(82892753);
+        });
+        it('should pass example 2', function () {
+            expect(Day14.searchMaximalReactionOutput(reactions2)).equals(5586022);
+        });
+    })
 });
--- src/days/day_14.ts
+++ src/days/day_14.ts
@@ -1,9 +1,25 @@
 export namespace Day14 {
-    export interface ReactionItem {amount: number; name: string;}
+    export const DEFAULT_INPUT = 'ORE';
+    export const DEFAULT_OUTPUT = 'FUEL';
+    export const DEFAULT_MAX_INPUT = 1000000000000;
+
+    export interface ReactionItem {
+        amount: number;
+        name: string;
+    }
 
     export interface ChemicalReaction {
         inputs: ReactionItem[];
         outputs: ReactionItem[];
+    }
+
+    export interface Residue {
+        total: number;
+        residue: number;
+    }
+
+    export interface ReagentList {
+        [reagent: string]: Residue;
     }
 
     export function parseChemicalItem(line: string): ReactionItem {
@@ -27,26 +43,268 @@ export namespace Day14 {
         };
     }
 
+    export function fetchReagent(reagentList: ReagentList, reagentName: string, amount: number,
+                                 addToTotal: boolean): number {
+        if (!reagentList[reagentName]) reagentList[reagentName] = {total: 0, residue: 0};
+        const reagent = reagentList[reagentName];
+        let requiredAmount = amount;
+        if (reagent.residue > 0) {
+            if (reagent.residue >= requiredAmount) {
+                reagent.residue -= requiredAmount;
+                requiredAmount = 0;
+            } else {
+                requiredAmount -= reagent.residue;
+                reagent.residue = 0;
+            }
+        }
+        if (requiredAmount > 0 && addToTotal) {
+            reagent.total += requiredAmount;
+            requiredAmount = 0;
+        }
+        return requiredAmount; // returning amount that still needs to be obtained
+    }
+
+    export function buildReactionTree(reactions: ChemicalReaction[]): string[][] {
+        const levels: string[][] = [[DEFAULT_INPUT]];
+        const reagentMemory: string[] = [DEFAULT_INPUT];
+
+        const getReactionsInLevel = (): ChemicalReaction[] => {
+            const levelReactions: ChemicalReaction[] = [];
+            for (let i = 0; i < reactions.length; i++) {
+                const reaction = reactions[i];
+                const outputName = reaction.outputs[0].name;
+                if (reagentMemory.indexOf(outputName) > -1) continue;
+
+                // only push reaction if all inputs are present in the memory
+                let hasAllInputs = true;
+                for (let ri = 0; ri < reaction.inputs.length; ri++) {
+                    const input = reaction.inputs[ri];
+                    if (reagentMemory.indexOf(input.name) === -1) {
+                        hasAllInputs = false;
+                        break;
+                    }
+                }
+                if (hasAllInputs) levelReactions.push(reaction);
+            }
+            return levelReactions;
+        };
+        let currentReactionLevel = getReactionsInLevel();
+        while (currentReactionLevel.length !== 0) {
+            const currentLevel: string[] = [];
+            currentReactionLevel.forEach(reaction => {
+                currentLevel.push(reaction.outputs[0].name);
+                reagentMemory.push(reaction.outputs[0].name);
+            });
+            levels.push(currentLevel);
+            currentReactionLevel = getReactionsInLevel();
+        }
+        return levels;
+    }
+
+    export interface ReagentMap {
+        [reagentName: string]: {
+            inputReactions: ChemicalReaction[];
+            outputReactions: ChemicalReaction[];
+        }
+    }
+
+    export function buildReagentMap(reactions: ChemicalReaction[]): ReagentMap {
+        const output: ReagentMap = {};
+        reactions.forEach(reaction => {
+            reaction.inputs.forEach(input => {
+                const reagentName = input.name;
+                if (!output[reagentName]) output[reagentName] = {inputReactions: [], outputReactions: []};
+                output[reagentName].inputReactions.push(reaction);
+            });
+            reaction.outputs.forEach(outputReaction => {
+                const reagentName = outputReaction.name;
+                if (!output[reagentName]) output[reagentName] = {inputReactions: [], outputReactions: []};
+                output[reagentName].outputReactions.push(reaction);
+            });
+        });
+        return output;
+    }
+
+    export function printReactionsInformation(reactions: ChemicalReaction[]) {
+        const bestReagentList = searchOptimalReactionCost(DEFAULT_INPUT, DEFAULT_OUTPUT, 1, reactions);
+        const reactionTree = Day14.buildReactionTree(reactions);
+        reactionTree.forEach(line => console.log(JSON.stringify(line)));
+
+        const reagentMap = Day14.buildReagentMap(reactions);
+        for (let level = 0; level < reactionTree.length; level++) {
+            console.log('___________________________________', level, '___________________________________');
+            const reactionTreeLevel = reactionTree[level];
+            reactionTreeLevel.forEach(reagentName => {
+                const reagent = reagentMap[reagentName];
+                console.log(reagentName, JSON.stringify(bestReagentList[reagentName]),
+                    'inputs', reagent.inputReactions.length,
+                    'outputs', reagent.outputReactions.length,
+                );
+            });
+        }
+    }
+
+    export function searchMaximalReactionOutput(reactions: ChemicalReaction[]): number {
+        const reagentList: ReagentList = {};
+        reagentList[DEFAULT_INPUT] = {total: DEFAULT_MAX_INPUT, residue: DEFAULT_MAX_INPUT};
+
+        const reagentMap = buildReagentMap(reactions);
+
+        function executeReaction(reaction: ChemicalReaction, amount: number): boolean {
+            const desiredOutput = reaction.outputs[0]; // always maximal 1 output
+            const reactionMultiplier = Math.ceil(amount / desiredOutput.amount);
+
+            for (let i = 0; i < reaction.inputs.length; i++) {
+                const inputReagent = reaction.inputs[i];
+                let requiredInputAmount = inputReagent.amount * reactionMultiplier;
+                requiredInputAmount = fetchReagent(reagentList, inputReagent.name, requiredInputAmount, false);
+                if (requiredInputAmount > 0) {
+                    if (inputReagent.name === DEFAULT_INPUT) return false;
+                    const createInputReaction = reagentMap[inputReagent.name].outputReactions[0]; // should exist
+                    if (!executeReaction(createInputReaction, requiredInputAmount)) {
+                        return false;
+                    }
+                    requiredInputAmount = fetchReagent(reagentList, inputReagent.name, requiredInputAmount, false);
+                    if (requiredInputAmount !== 0) throw new Error('something went wrong');
+                }
+            }
+            reaction.outputs.forEach(outputReagent => {
+                if (!reagentList[outputReagent.name]) reagentList[outputReagent.name] = {total: 0, residue: 0};
+                const outputAmount = outputReagent.amount * reactionMultiplier;
+                reagentList[outputReagent.name].total += outputAmount;
+                reagentList[outputReagent.name].residue += outputAmount;
+            });
+            return true;
+        }
+
+        const fuelReaction = reagentMap[DEFAULT_OUTPUT].outputReactions[0];
+        let fuelCounter = 0;
+        const optimalReaction = searchOptimalReactionCost(DEFAULT_INPUT, DEFAULT_OUTPUT, 1, reactions);
+        const maxOptimalOutput = Math.floor(DEFAULT_MAX_INPUT / optimalReaction[DEFAULT_INPUT].total);
+        executeReaction(fuelReaction, maxOptimalOutput);
+        fuelCounter = maxOptimalOutput;
+        while (executeReaction(fuelReaction, 1)) {
+            fuelCounter++;
+        }
+
+        return fuelCounter;
+    }
+
     export function searchOptimalReactionCost(inputName: string, outputName: string, outputAmount: number,
-                                              reactions: ChemicalReaction[], currentBestCost: number = null): number {
-        let bestCost: number = null;
+                                              reactions: ChemicalReaction[], reagentList: ReagentList = {}
+    ): ReagentList {
+        let bestTotalInput: number = null;
+        let bestReagentList: ReagentList = null;
+
         for (let i = 0; i < reactions.length; i++) {
             const reaction = reactions[i];
             const desiredOutput = reaction.outputs.find(x => x.name === outputName);
             if (!desiredOutput) continue;
 
-            let cost = 0;
-            const reactionMultiplier = Math.ceil(outputAmount / desiredOutput.amount);
+            let currentReagentList: ReagentList = JSON.parse(JSON.stringify(reagentList));
+            const requiredOutputAmount = fetchReagent(currentReagentList, outputName, outputAmount, false);
+            const reactionMultiplier = Math.ceil(requiredOutputAmount / desiredOutput.amount);
+            const totalOutputAmount = desiredOutput.amount * reactionMultiplier;
+            const residueOutputAmount = totalOutputAmount - requiredOutputAmount;
 
             for (let c = 0; c < reaction.inputs.length; c++) {
                 const input = reaction.inputs[c];
-                const requiredInputAmount = reactionMultiplier * input.amount;
-                if (input.name !== inputName) cost += searchOptimalReactionCost(inputName, input.name, requiredInputAmount, reactions, bestCost);
-                else cost += requiredInputAmount;
-                if (bestCost !== null && cost > bestCost) break;
+                let requiredInputAmount = reactionMultiplier * input.amount;
+                // use any residue available
+                if (currentReagentList[input.name] && currentReagentList[input.name].residue) {
+                    requiredInputAmount = fetchReagent(currentReagentList, inputName, requiredInputAmount, false);
+                }
+                // fetch input resources
+                if (requiredInputAmount !== 0) {
+                    if (input.name !== inputName) currentReagentList = searchOptimalReactionCost(inputName, input.name,
+                        requiredInputAmount, reactions, currentReagentList);
+                    else {
+                        fetchReagent(currentReagentList, inputName, requiredInputAmount, true);
+                    }
+                }
             }
-            if (bestCost === null || cost < bestCost) bestCost = cost;
+
+            // add missing output to the reagent list
+            fetchReagent(currentReagentList, outputName, totalOutputAmount, true);
+            currentReagentList[outputName].residue += residueOutputAmount;
+
+            const totalInput = currentReagentList[inputName].total;
+            if (bestTotalInput === null || totalInput < bestTotalInput) {
+                bestTotalInput = totalInput;
+                bestReagentList = currentReagentList;
+            }
         }
-        return bestCost;
+
+        return bestReagentList;
     }
+}
+
+if (!module.parent) {
+    const lines = [
+        '15 RNMTG => 6 QSXV',
+        '21 MKJN => 9 KDFZ',
+        '1 KVFL, 4 NZWL => 3 FHDT',
+        '1 FZJXD, 2 SWZK, 1 QRLRS => 6 ZRNK',
+        '8 KVFL => 6 SBZKF',
+        '11 DXFB, 1 CPBXJ, 8 TXFCS, 1 ZPMHL, 1 BCHTD, 2 FZJXD, 2 WKZMQ, 1 NZWL => 8 MPLJ',
+        '5 KDFZ, 1 QSXV => 9 TXFCS',
+        '1 PMLGM, 21 CKVN => 3 KVFL',
+        '1 XFRLH, 3 QRLRS => 4 CKVN',
+        '5 KBJS, 15 XFRLH, 6 WZPZX, 15 KVFL, 4 DXFB, 4 ZPMHL, 11 JCKCK, 26 KFGPB => 9 BWVS',
+        '10 KNRDW, 2 XCML => 9 BCNL',
+        '26 LBLH => 9 KBJS',
+        '5 DTFBQ, 4 PJTD => 6 FHKSW',
+        '6 HTRFP, 1 FVXV, 4 JKLNF, 1 TXFCS, 2 PXBP => 4 JRBFT',
+        '21 DTFBQ => 9 JGQJ',
+        '2 KBJS => 3 FZJXD',
+        '24 LBLH => 6 QFMTZ',
+        '1 CBNJT => 7 LSCW',
+        '5 KVFL => 2 NZWL',
+        '12 DNHL, 4 BCNL => 4 LBLH',
+        '15 RHVG => 1 PJCWT',
+        '4 KDFZ, 1 KVFL => 3 BCHTD',
+        '2 XFDW, 7 BCHTD => 7 WKZMQ',
+        '2 SBZKF, 1 PLTX => 3 DXFB',
+        '1 PLTX, 11 HTRFP, 6 PMLGM => 1 JCKCK',
+        '1 TQCX, 10 DNHL => 8 DTFBQ',
+        '2 TQCX, 2 KTBFB => 5 RHVG',
+        '8 MVFW => 3 CPBXJ',
+        '148 ORE => 4 CBNJT',
+        '9 CPBXJ, 5 DTFBQ => 6 PMLGM',
+        '11 ZXCF, 15 PJCWT, 4 FZJXD => 7 PJTD',
+        '1 JGQJ => 6 DCBNV',
+        '4 LSCW, 16 BCNL => 7 MVFW',
+        '1 RHVG => 4 XFDW',
+        '8 MPLJ, 16 JRBFT, 43 KBJS, 11 NZWL, 4 BWVS, 22 ZPMHL => 1 FUEL',
+        '1 QFMTZ, 3 CKVN => 5 PLTX',
+        '5 CKVN, 10 SWZK => 7 HTRFP',
+        '2 PXBP, 1 QRLRS, 7 KTBFB => 7 NDZGV',
+        '1 QRLRS, 9 KBJS, 2 TQCX => 2 SWZK',
+        '9 TZKZ, 3 ZRNK, 4 PXBP => 4 FVXV',
+        '1 PMLGM, 1 SWZK, 6 FZJXD => 7 MKJN',
+        '16 MVFW, 2 KBJS => 7 ZXCF',
+        '1 MVFW => 6 HVGF',
+        '1 LSCW, 1 HVGF => 8 RNMTG',
+        '5 ZRNK, 1 TQCX => 3 PXBP',
+        '130 ORE => 5 KNRDW',
+        '1 RHVG, 2 KFGPB, 1 LSCW => 7 QRLRS',
+        '6 XFRLH => 8 TZKZ',
+        '24 HVGF, 8 KTBFB => 1 XFRLH',
+        '2 KNRDW, 2 CBNJT => 6 DNHL',
+        '1 FHDT => 4 JKLNF',
+        '1 QSXV, 10 XFGZX, 2 DCBNV => 8 ZPMHL',
+        '1 FHDT, 7 NDZGV => 4 WZPZX',
+        '11 FHKSW => 5 XFGZX',
+        '10 LSCW => 8 KTBFB',
+        '133 ORE => 1 XCML',
+        '8 XCML => 4 TQCX',
+        '6 CPBXJ, 8 CBNJT => 6 KFGPB',
+    ];
+    const reactions = lines.map(Day14.parseChemicalReaction);
+    const bestReagentList = Day14.searchOptimalReactionCost('ORE', 'FUEL', 1, reactions);
+    console.log('part 1', bestReagentList['ORE'].total);
+
+    // Day14.printReactionsInformation(reactions);
+
+    console.log('part 2', Day14.searchMaximalReactionOutput(reactions));
 }
