b316234560441fc0a8c424aee09a0f457bded228
hendrik <hamerlinckhendrik@gmail.com>
2023-01-31 16:31:49 +0100

    day 6 - part 1

--- aoc-2022-rust/src/bin/day6/main.rs
+++ aoc-2022-rust/src/bin/day6/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Index;
+use std::slice::SliceIndex;
 use aoc_lib::engine::input_engine::{read_day_input, read_day_input_example};
 
 fn parse_input(input: &Vec<String>) -> usize {
@@ -5,8 +7,20 @@ fn parse_input(input: &Vec<String>) -> usize {
 }
 
 fn part_1(input: &Vec<String>) {
-    let result = parse_input(input);
-    println!("{}", result)
+    let result = input.index(0);
+    let mut fragment: Vec<char> = Vec::new();
+    let mut parsed: usize = 0;
+    for (i, c) in result.chars().into_iter().enumerate() {
+        if fragment.contains(&c) {
+            fragment = Vec::new();
+        }
+        fragment.push(c);
+        if fragment.len() >= 4 {
+            parsed = i + 1;
+            break;
+        }
+    }
+    println!("{}", parsed);
 }
 
 fn part_2(input: &Vec<String>) {
