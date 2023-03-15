126552618acfabb7051ce3c1d91b8d341f5f922e
hendrik <hamerlinckhendrik@gmail.com>
2023-03-15 22:07:47 +0100

    WIP day 11 cleanup

--- aoc-2022-rust/src/bin/day11/main.rs
+++ aoc-2022-rust/src/bin/day11/main.rs
@@ -1,4 +1,3 @@
-use aoc_lib::engine::input_engine::{read_day_input, read_day_input_example};
 use regex::Regex;
 
 // required library https://docs.rs/eval/latest/eval/
@@ -23,14 +22,12 @@ struct MonkeyTest {
 
 
 fn parse_input(input: &str) -> usize {
-    // let monkey_regex = Regex::new(r"Monkey (\d+):\s+Starting items: ((\d,?\s?)*)\s+Operation:([^\n]+)\s+Test: divisible by (\d+)\s+If true: throw to monkey (\d+)\s+If false: throw to monkey (\d+)").unwrap();
     let monkey_regex = Regex::new(r"Monkey (\d+):\s+Starting items: ((\d+(,\s)?)*)\s+Operation: ([^\n]+)\s+Test: divisible by (\d+)\s+If true: throw to monkey (\d+)\s+If false: throw to monkey (\d+)").unwrap();
-    // let matches = monkey_regex.find_iter(input).collect::VecM<>();
     monkey_regex.find_iter(input).for_each(|monkey_match|  {
         let captures = monkey_regex.captures(monkey_match.as_str()).unwrap();
-        let monkey_id = &captures[1];
+        let monkey_id = &captures[1]; // start at 1, 0 is the full capture
         let starting_items = &captures[2];
-        let operation_raw = &captures[5]; // 2 subgroups in the items group 2 -> 5
+        let operation_raw = &captures[5]; // 2 subgroups in the starting_items group 2 -> 5
         let divider = &captures[6];
         let true_monkey_id = &captures[7];
         let false_monkey_id = &captures[8];
@@ -50,15 +47,15 @@ fn part_1(input: &str) {
 // }
 
 fn main() {
-    let rawInputExample = include_str!("input.example");
-    let rawInput = include_str!("input");
+    let raw_input_example = include_str!("input.example");
+    let raw_input = include_str!("input");
 
     println!("Part 1 - example input");
-    part_1(&rawInputExample);
+    part_1(raw_input_example);
 
-    // println!("Part 1 - input");
-    // part_1(&input);
-    //
+    println!("Part 1 - input");
+    part_1(raw_input);
+
     // println!("Part 2 - example input");
     // part_2(&example_input);
     //
