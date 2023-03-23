4146a52d9bd1a29aafe86d4ac4321da3e82e4efe
hendrik <hamerlinckhendrik@gmail.com>
2023-03-23 09:36:41 +0100

    day 11 - parse monkeys correctly

--- aoc-2022-rust/src/bin/day11/main.rs
+++ aoc-2022-rust/src/bin/day11/main.rs
@@ -3,13 +3,13 @@ use regex::Regex;
 // required library https://docs.rs/eval/latest/eval/
 
 struct Item {
-    id: u32,
+    worry_level: u32,
     monkey_id: Option<u32>,
 }
 
 struct Monkey {
     id: u32,
-    starting_items: Vec<usize>,
+    items: Vec<Item>,
     operation_expression: String,
     monkey_test: MonkeyTest,
 }
@@ -23,16 +23,34 @@ struct MonkeyTest {
 
 fn parse_input(input: &str) -> usize {
     let monkey_regex = Regex::new(r"Monkey (\d+):\s+Starting items: ((\d+(,\s)?)*)\s+Operation: ([^\n]+)\s+Test: divisible by (\d+)\s+If true: throw to monkey (\d+)\s+If false: throw to monkey (\d+)").unwrap();
-    monkey_regex.find_iter(input).for_each(|monkey_match|  {
-        let captures = monkey_regex.captures(monkey_match.as_str()).unwrap();
-        let monkey_id = &captures[1]; // start at 1, 0 is the full capture
-        let starting_items = &captures[2];
+    let monkeys = monkey_regex.captures_iter(input).map(|captures| {
+        let monkey_id = captures[1].parse::<u32>().unwrap(); // start at 1, 0 is the full capture
+        let raw_starting_items = captures[2].to_string();
+        let mut monkey_items: Vec<Item> = vec![];
+        raw_starting_items
+            .split(", ")
+            .for_each(|value| {
+                let item = Item {
+                    worry_level: value.parse::<u32>().unwrap(),
+                    monkey_id: Some(monkey_id),
+                };
+                monkey_items.push(item);
+            });
         let operation_raw = &captures[5]; // 2 subgroups in the starting_items group 2 -> 5
-        let divider = &captures[6];
-        let true_monkey_id = &captures[7];
-        let false_monkey_id = &captures[8];
-        println!("{} - {} - {} - {} - {} - {}", monkey_id, starting_items, operation_raw, divider, true_monkey_id, false_monkey_id)
-    });
+        let divider = captures[6].parse::<u32>().unwrap();
+        let true_monkey_id = captures[7].parse::<u32>().unwrap();
+        let false_monkey_id = captures[8].parse::<u32>().unwrap();
+        Monkey {
+            id: monkey_id,
+            monkey_test: MonkeyTest {
+                divider,
+                false_monkey_id,
+                true_monkey_id,
+            },
+            operation_expression: operation_raw.to_string(),
+            items: monkey_items,
+        }
+    }).collect::<Vec<Monkey>>();
     input.len()
 }
 
