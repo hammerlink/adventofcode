0a8e23704892914d46072fa68bf3a48f7a2a0341
hammerlink <hendrik.hamerlinck@hammernet.be>
2023-10-12 20:34:41 +0200

    use correct test names

--- aoc-2022-rust/src/days/day_15/main_15.rs
+++ aoc-2022-rust/src/days/day_15/main_15.rs
@@ -112,14 +112,14 @@ fn part_2(input: &str, max_position: isize) -> usize {
 }
 
 #[test]
-fn day_12_part_1_example() {
+fn day_15_part_1_example() {
     let raw_input_example = include_str!("input.example");
     let result = part_1(raw_input_example, 10);
     println!("{}", result);
     assert_eq!(result, 26);
 }
 #[test]
-fn day_12_part_1() {
+fn day_15_part_1() {
     let input = include_str!("input");
     let result = part_1(input, 2000000);
     println!("{}", result);
@@ -127,12 +127,12 @@ fn day_12_part_1() {
     assert_eq!(result, 4883971);
 }
 #[test]
-fn day_12_part_2_example() {
+fn day_15_part_2_example() {
     let input = include_str!("input.example");
     assert_eq!(part_2(input, 20), 56000011);
 }
 #[test]
-fn day_12_part_2() {
+fn day_15_part_2() {
     let input = include_str!("input");
     let result = part_2(input, 4000000);
     println!("{}", result);
