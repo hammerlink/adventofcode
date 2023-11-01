46c392691ce97231726101dbe977212a1e8bd0b8
hammerlink <hendrik.hamerlinck@hammernet.be>
2023-11-01 20:47:00 +0100

    2022 day 17 part 2 js

--- src/2022/y2022_day17.ts
+++ src/2022/y2022_day17.ts
@@ -122,38 +122,103 @@ export namespace Y2022_Day17 {
         };
     }
 
-    export function part1(jetSequence: string): number {
+    type Repitition = { jetIndex: number; height: number, index: number };
+
+    function checkForRepititions(sequences: Repitition[]): Repitition[] {
+        // must be higher then 4, requires 3 complete repititions
+        const endCheckPoint = Math.floor(sequences.length / 2);
+        for (let i = sequences.length - 5; i > endCheckPoint; i--) {
+            const delta = sequences.length - 1 - i;
+            if (i - 2 * delta < 0) continue;
+
+            const repitition: Repitition[] = [];
+            let isSuccess = true;
+            for (let j = 0; j < delta; j++) {
+                const endSequence = sequences[sequences.length - 1 - j];
+                if (endSequence.jetIndex !== sequences[i - j].jetIndex) {
+                    isSuccess = false;
+                    break;
+                }
+                repitition.push(endSequence);
+            }
+            if (isSuccess) {
+                let secondSuccess = true;
+                for (let j = 0; j < delta; j++) {
+                    const endSequence = sequences[sequences.length - 1 - j];
+                    if (endSequence.jetIndex !== sequences[i - delta - j].jetIndex) {
+                        secondSuccess = false;
+                        break;
+                    }
+                }
+                // check for second repititon
+                if (secondSuccess) return repitition;
+            }
+        }
+        return null;
+    }
+
+    export function part1(jetSequence: string, iterations = 2022): number {
         jetSequence = jetSequence.replace(/[^<>]/g, '');
-        console.log(jetSequence, jetSequence.length)
-        const map = MapEngine.newMap<TowerCell>();
+        console.log(jetSequence, jetSequence.length);
+        const map: TowerMap = {
+            ...MapEngine.newMap<TowerCell>(),
+            lastBlocks: [],
+        };
+        for (let i = 0; i < 7; i++) map.lastBlocks.push(0);
         for (let x = -1; x < 8; x++) MapEngine.setPointInMap(map, x, 0, {isRock: true});
         let rockIndex = 0;
         let jetIndex = 0;
-        for (let i = 0; i < 2022; i++) {
+        let sequenceRepetition: Repitition[] = [];
+        let foundRepetition: Repitition[] | null = null;
+        let repetitionCounter = 0;
+        let repetitionJumpExecuted = false;
+        let extraHeight = 0;
+        for (let i = 0; i < iterations; i++) {
+            if (!repetitionJumpExecuted && rockIndex === 0) {
+                sequenceRepetition.push({jetIndex, height: -map.minY, index: i});
+                let delta = 0;
+                if (foundRepetition) {
+                    delta = sequenceRepetition[sequenceRepetition.length - 1].height - sequenceRepetition[sequenceRepetition.length - 1 - foundRepetition.length].height;
+                    repetitionCounter++;
+                    if (repetitionCounter > foundRepetition.length * 3) {
+                        repetitionJumpExecuted = true;
+                        const deltaI = sequenceRepetition[sequenceRepetition.length - 1].index - sequenceRepetition[sequenceRepetition.length - 1 - foundRepetition.length].index;
+                        const repetitionAmount = Math.floor((iterations - i) / (deltaI));
+                        i += deltaI * repetitionAmount;
+                        extraHeight = repetitionAmount * delta;
+                    }
+                }
+                foundRepetition = checkForRepititions(sequenceRepetition);
+                console.log('iteration', i, 'jetIndex', jetIndex, -map.minY, delta);
+            }
             const {rock, rockIndex: newRockIndex} = spawnRock(map, rockIndex);
             rockIndex = newRockIndex;
             const {jetIndex: newJetIndex} = dropRock(map, rock, jetSequence, jetIndex, i);
             jetIndex = newJetIndex;
         }
 
-
         map.maxY = map.minY + 10;
         printMap(map, 2022);
 
-        return -map.minY;
+        return -map.minY + extraHeight;
     }
 
     export function part2(line: string): number {
-        return 0;
+        // would take to long, pattern recognition?
+        // sequence gets repeated, rocks get repeated, floor not necessarily so...?
+        // after each rock iteration check the sequence iteration
+        // there definitely is a repetition
+        return part1(line, 1000000000000);
     }
 }
 
 if (!module.parent) {
     async function main() {
-        const exampleSequence = fs.readFileSync(path.join(path.dirname(__filename), './data/y2022_day17.example')).toString();
+        const exampleSequence = fs
+            .readFileSync(path.join(path.dirname(__filename), './data/y2022_day17.example'))
+            .toString();
 
         assert.equal(Y2022_Day17.part1(exampleSequence), 3068, 'example 1 part 1');
-        assert.equal(Y2022_Day17.part2(exampleSequence), 0, 'example 1 part 2');
 
         const inputSequence = fs
             .readFileSync(path.join(path.dirname(__filename), './data/y2022_day17.input'))
