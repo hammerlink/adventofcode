95914a2e1df0d5e3879ee7f6be3cf8f03c0dfa21
hammerlink <hendrik.hamerlinck@hammernet.be>
2023-12-03 07:18:27 +0100

    2023 day 3 - part 1 complete

--- src/2023/y2023_day03.ts
+++ src/2023/y2023_day03.ts
@@ -1,18 +1,29 @@
 import { FileEngine } from '../engine/file.engine';
 import * as assert from 'assert';
 
-export namespace Y2023_Day02 {
+export namespace Y2023_Day03 {
     function sliceHasSymbol(slice: string) {
-        return !slice.match(/([0-9]|\.)/);
+        return !!slice.match(/[^0-9\.]/);
     }
     export function part1(lines: string[]): number {
+        let total = 0;
         for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
             const line = lines[lineIndex];
-            const numberMatches = line.matchAll(/[0-9]+/g);
-            console.log(JSON.stringify(numberMatches));
-            //
+            const numberMatches = Array.from(line.matchAll(/[0-9]+/g));
+            numberMatches.forEach(numberMatch => {
+                const index = numberMatch.index;
+                const rawValue = numberMatch[0]; 
+                let beginIndex = index - 1;
+                if (beginIndex < 0) beginIndex = 0;
+                let endIndex = index + rawValue.length + 1; // endIndex is not including for js
+                let slice = line.slice(beginIndex, endIndex);
+                if (lineIndex > 0) slice += lines[lineIndex - 1].slice(beginIndex, endIndex);
+                if (lineIndex < (lines.length - 1)) slice += lines[lineIndex + 1].slice(beginIndex, endIndex);
+                const hasSymbol = sliceHasSymbol(slice);
+                if (hasSymbol) total += parseInt(rawValue);
+            })
         }
-        return 0;
+        return total;
     }
     export function part2(lines: string[]): number {
         return 0;
@@ -24,27 +35,27 @@ if (!module.parent) {
 
     async function main() {
         const exampleLines = await FileEngine.readFileToLines(
-            path.join(path.dirname(__filename), './data/y2023_day02.example'),
+            path.join(path.dirname(__filename), './data/y2023_day03.example'),
             false,
         );
         const lines = await FileEngine.readFileToLines(
-            path.join(path.dirname(__filename), './data/y2023_day02.input'),
+            path.join(path.dirname(__filename), './data/y2023_day03.input'),
             false,
         );
 
-        assert.equal(Y2023_Day02.part1(exampleLines), 4361, 'example 1 part 1');
+        assert.equal(Y2023_Day03.part1(exampleLines), 4361, 'example 1 part 1');
 
         // part 1
         let startMs = Date.now();
-        const part1Result = Y2023_Day02.part1(lines);
+        const part1Result = Y2023_Day03.part1(lines);
         console.log('part 1', part1Result, 'ms', Date.now() - startMs);
         // assert.equal(part1Result, 2237);
 
         // part 2
-        // assert.equal(Y2023_Day02.part2(exampleLines), 2286, 'example 1 part 2');
+        // assert.equal(Y2023_Day03.part2(exampleLines), 2286, 'example 1 part 2');
 
         // startMs = Date.now();
-        // const part2Result = Y2023_Day02.part2(lines);
+        // const part2Result = Y2023_Day03.part2(lines);
         // assert.equal(part2Result, 66681);
         // console.log('part 2', part2Result, 'ms', Date.now() - startMs);
     }
