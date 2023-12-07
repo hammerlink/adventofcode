aa51468b819e2d18954baa60758b19a50d8ffc26
hammerlink <hendrik.hamerlinck@hammernet.be>
2023-12-07 05:59:38 +0100

    prep day 7

---
+++ src/2023/y2023_day07.ts
@@ -0,0 +1,45 @@
+import { FileEngine } from '../engine/file.engine';
+import * as assert from 'assert';
+
+export namespace Y2023_Day07 {
+    export function part1(lines: string[]): number {
+        return 0;
+    }
+
+    export function part2(lines: string[]): number {
+        return 0;
+    }
+}
+
+if (!module.parent) {
+    const path = require('path');
+
+    async function main() {
+        const exampleLines = await FileEngine.readFileToLines(
+            path.join(path.dirname(__filename), './data/y2023_day07.example'),
+            false,
+        );
+        const lines = await FileEngine.readFileToLines(
+            path.join(path.dirname(__filename), './data/y2023_day07.input'),
+            false,
+        );
+
+        assert.equal(Y2023_Day07.part1(exampleLines), 0, 'example 1 part 1');
+
+        // part 1
+        let startMs = Date.now();
+        const part1Result = Y2023_Day07.part1(lines);
+        console.log('part 1', part1Result, 'ms', Date.now() - startMs);
+        // assert.equal(part1Result, 2065338);
+
+        // part 2
+        assert.equal(Y2023_Day07.part2(exampleLines), 0, 'example 1 part 2');
+
+        startMs = Date.now();
+        const part2Result = Y2023_Day07.part2(lines);
+        console.log('part 2', part2Result, 'ms', Date.now() - startMs);
+        // assert.equal(part2Result, 0);
+    }
+
+    main().catch((err) => console.error(err));
+}
