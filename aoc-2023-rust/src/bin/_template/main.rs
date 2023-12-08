c18a581a0bfaa73ae2912e3039a01c934e24a8ed
hammerlink <hendrik.hamerlinck@hammernet.be>
2023-12-08 17:13:11 +0100

    add rust template

---
+++ aoc-2023-rust/src/bin/_template/main.rs
@@ -0,0 +1,24 @@
+fn part_1(input: &str) -> usize {
+    0
+}
+
+fn part_2(input: &str) -> usize {
+    0
+}
+#[allow(dead_code)]
+fn main() {
+    let example_input = include_str!("../../../../src/2023/data/y2023_day01.input");
+    let input = include_str!("../../../../src/2023/data/y2023_day01.input");
+    let part_1_example = part_1(example_input);
+    assert_eq!(part_1_example, 0, "example part 1 not correct");
+    let part_1_result = part_1(input);
+    assert_eq!(part_1_result, 0, "part 1 not correct");
+    println!("part 1 - {}", part_1_result);
+
+    let part_2_example = part_2(example_input);
+    assert_eq!(part_2_example, 0, "example part 2 not correct");
+
+    let part_2_result = part_2(input);
+    assert_eq!(part_2_result, 0, "part 2 not correct");
+    println!("part 2 - {}", part_2_result);
+}
