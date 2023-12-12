0f08bc5930b07d1fd1e7bc9501cc1b98fe36dc08
hammerlink <hendrik.hamerlinck@hammernet.be>
2023-12-12 06:27:41 +0100

    day 12 part 1

--- src/2023/y2023_day12.ts
+++ src/2023/y2023_day12.ts
@@ -2,9 +2,49 @@ import { FileEngine } from '../engine/file.engine';
 import * as assert from 'assert';
 
 export namespace Y2023_Day12 {
+    type SpringLine = {
+        /** . ? # */
+        line: string;
+        checkLine: number[];
+    };
+    function parseSpringLine(inputLine: string): SpringLine {
+        const pieces = inputLine.split(' ');
+        const line: string = pieces[0];
+        const checkLine = pieces[1].split(',').map((x) => parseInt(x, 10));
+        return { checkLine, line };
+    }
+    function validateSpringLine({ line, checkLine }: SpringLine): boolean {
+        if (line.includes('?')) throw new Error(`not complete yet ${line}`);
+        const linePieces = line
+            .replace(/\.+/g, '|')
+            .split('|')
+            .filter((x) => x.length);
+        if (checkLine.length !== linePieces.length) return false;
+        for (let i = 0; i < checkLine.length; i++) {
+            if (checkLine[i] !== linePieces[i].length) return false;
+        }
+        return true;
+    }
+    function tryAllCombinations(springLine: SpringLine): number {
+        const nextIndex = springLine.line.indexOf('?');
+        if (nextIndex === -1) {
+            const isValid = validateSpringLine(springLine);
+            return isValid ? 1 : 0;
+        }
+        let counter = 0;
+        const rawLine = [...springLine.line];
+        rawLine[nextIndex] = '.';
+        springLine.line = rawLine.join('');
+        counter += tryAllCombinations(springLine);
+        rawLine[nextIndex] = '#';
+        springLine.line = rawLine.join('');
+        counter += tryAllCombinations(springLine);
+        return counter;
+    }
 
     export function part1(lines: string[]): number {
-        return 0;
+        const springLines = lines.map(parseSpringLine);
+        return springLines.reduce((t, springLine) => t + tryAllCombinations(springLine), 0);
     }
 
     export function part2(lines: string[]): number {
@@ -25,7 +65,8 @@ if (!module.parent) {
             false,
         );
 
-        assert.equal(Y2023_Day12.part1(exampleLines), 0, 'example part 1');
+        console.log('part 1 example start');
+        assert.equal(Y2023_Day12.part1(exampleLines), 21, 'example part 1');
 
         // part 1
         let startMs = Date.now();
