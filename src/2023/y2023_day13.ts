11e6f35158f1c888ad512fb90ec405d7aaca1ddf
hammerlink <hendrik.hamerlinck@hammernet.be>
2023-12-15 21:12:39 +0100

    day 13 - part 2

--- src/2023/y2023_day13.ts
+++ src/2023/y2023_day13.ts
@@ -13,47 +13,48 @@ export namespace Y2023_Day13 {
         }
         return output;
     }
-    function getMirrorIndex(lines: string[]): number | undefined {
-        console.log();
-        lines.forEach((line) => console.log(line));
+    function getMirrorIndex(lines: string[], differences: number): number | undefined {
+        // console.log();
+        // lines.forEach((line) => console.log(line));
         const max = lines[0].length;
         for (let i = 1; i < max; i++) {
-            let isTotalMirror = true;
+            let differenceCounter = 0;
             for (let y = 0; y < lines.length; y++) {
                 const line = lines[y];
                 const leftPart = line.substring(0, i);
                 const rightPart = line.substring(i);
-                const isMirror =
-                    leftPart.length > rightPart.length
-                        ? leftPart.endsWith(rightPart.split('').reverse().join(''))
-                        : rightPart.startsWith(leftPart.split('').reverse().join(''));
-                if (!isMirror) {
-                    isTotalMirror = false;
-                    break;
-                }
+                const lineDifference = calculateDifferences(leftPart, rightPart);
+                differenceCounter += lineDifference;
+                if (differenceCounter > differences) break;
             }
-            if (isTotalMirror) return i;
+            if (differences === differenceCounter) return i;
         }
 
         return undefined;
     }
-    export function calculateMirrorValue(lines: string[]): number {
+    function calculateDifferences(leftPart: string, rightPart: string): number {
+        let counter = 0;
+        const maxLen = Math.min(leftPart.length, rightPart.length);
+        for (let i = 0; i < maxLen; i++) {
+            if (leftPart[leftPart.length - 1 - i] !== rightPart[i]) counter++;
+        }
+        return counter;
+    }
+    export function calculateMirrorValue(lines: string[], differences: number): number {
         const originalLines = lines;
         // return directly
-        const horizontalMirrorIndex = getMirrorIndex(lines);
+        const horizontalMirrorIndex = getMirrorIndex(lines, differences);
         if (horizontalMirrorIndex !== undefined) return horizontalMirrorIndex;
 
         // multiply 100
         lines = invertLines(lines);
-        const verticalMirrorIndex = getMirrorIndex(lines);
+        const verticalMirrorIndex = getMirrorIndex(lines, differences);
         if (verticalMirrorIndex === undefined) {
             console.log();
             originalLines.forEach((line) => console.log(line));
             throw new Error('no mirror found');
         }
         return verticalMirrorIndex * 100;
-
-        return 0;
     }
     function iterateMaps(lines: string[]): string[][] {
         const maps: string[][] = [];
@@ -71,11 +72,12 @@ export namespace Y2023_Day13 {
     }
     export function part1(lines: string[]): number {
         const maps = iterateMaps(lines);
-        return maps.reduce((t, mapLines) => t + calculateMirrorValue(mapLines), 0);
+        return maps.reduce((t, mapLines) => t + calculateMirrorValue(mapLines, 0), 0);
     }
 
     export function part2(lines: string[]): number {
-        return 0;
+        const maps = iterateMaps(lines);
+        return maps.reduce((t, mapLines) => t + calculateMirrorValue(mapLines, 1), 0);
     }
 }
 
@@ -101,7 +103,7 @@ if (!module.parent) {
 ..#.#.#.....##.##
 .#.#..##.##..##..
 .#.#..##.##..##..`;
-        Y2023_Day13.calculateMirrorValue(issue.split('\n'));
+        Y2023_Day13.calculateMirrorValue(issue.split('\n'), 0);
         console.log('part 1 example start');
         assert.equal(Y2023_Day13.part1(exampleLines), 405, 'example part 1');
 
@@ -110,9 +112,9 @@ if (!module.parent) {
         console.log('part 1 start');
         const part1Result = Y2023_Day13.part1(lines);
         console.log('part 1', part1Result, 'ms', Date.now() - startMs);
-        assert.equal(part1Result, 0);
+        assert.equal(part1Result, 42974);
         // part 2
-        assert.equal(Y2023_Day13.part2(exampleLines), 0, 'example part 2');
+        assert.equal(Y2023_Day13.part2(exampleLines), 400, 'example part 2');
 
         startMs = Date.now();
         console.log('part 2 start');
