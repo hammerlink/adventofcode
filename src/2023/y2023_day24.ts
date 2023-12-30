efdbb8b652bbdf3c4b884fb05b5add9e0a71b7e8
hammerlink <hendrik.hamerlinck@hammernet.be>
2023-12-30 21:34:49 +0100

    2023 day 24 - part 2, with performance issues

--- src/2023/y2023_day24.ts
+++ src/2023/y2023_day24.ts
@@ -10,6 +10,7 @@ export namespace Y2023_Day24 {
     export type BaseHailStone = {
         position: Location;
         direction: Location;
+        parameters2d: Parameters2D | null;
     };
     export type HailStone = BaseHailStone & {
         x0: Location;
@@ -18,19 +19,48 @@ export namespace Y2023_Day24 {
         y0Timing: number;
         z0: Location;
         z0Timing: number;
-        parameters2d: Parameters2D | null;
-        parameters3d: Parameters3D | null;
     };
     type Intersection2D = {
         x: number;
         y: number;
         durationA: number;
         durationB: number;
-        a: HailStone;
-        b: HailStone;
+        a: BaseHailStone;
+        b: BaseHailStone;
     };
     type Parameters2D = { a: number; b: number };
-    type Parameters3D = { a: number; b: number; c: number };
+    type Rock = {
+        position: Location;
+        direction: Location;
+        parameters2d: Parameters2D | null;
+        hailStoneCollisions: Array<HailStone & { duration: number; index: number }>;
+        timeNs: number;
+    };
+
+    function getNextCollision(
+        rock: BaseHailStone,
+        hailStone: BaseHailStone,
+        duration: number,
+    ): { position: Location; duration: number } | null {
+        const intersection = get2DIntersection(rock, hailStone);
+        if (
+            intersection === null ||
+            intersection.durationA < duration ||
+            intersection.durationA !== intersection.durationB ||
+            Math.round(intersection.durationA) !== intersection.durationA
+        )
+            return null;
+        const rockPosition = applyDuration(rock, intersection.durationA);
+        const hailStonePosition = applyDuration(hailStone, intersection.durationB);
+        if (
+            rockPosition.x !== hailStonePosition.x ||
+            rockPosition.y !== hailStonePosition.y ||
+            rockPosition.z !== hailStonePosition.z
+        )
+            return null;
+        return { duration: intersection.durationA, position: rockPosition };
+    }
+
     export function parseHailStone(line: string): HailStone {
         const pieces = line.split(' @ ').map((x) => x.trim());
         assert.equal(pieces.length, 2);
@@ -60,7 +90,6 @@ export namespace Y2023_Day24 {
             z0: z0.location,
             z0Timing: z0.duration,
             parameters2d: get2DParameters(baseHailStone),
-            parameters3d: get3DParameters({ ...baseHailStone, x0: x0.location, y0: y0.location }),
         };
     }
     function get2DParameters(hailStone: { position: Location; direction: Location }): Parameters2D | null {
@@ -72,14 +101,6 @@ export namespace Y2023_Day24 {
         const a = -b / y0Location.location.x;
 
         return { a, b };
-    }
-    export function applyFormulaDuration(hailStone: HailStone, duration: number): Location {
-        const x = hailStone.position.x + hailStone.direction.x * duration;
-        const y = hailStone.position.y + hailStone.direction.y * duration;
-        const z = hailStone.position.z + hailStone.direction.z * duration;
-        const formulaZ = x * hailStone.parameters3d.a + y * hailStone.parameters3d.b + hailStone.parameters3d.c;
-        assert.equal(formulaZ, z);
-        return { x, y, z };
     }
     export function applyDuration(hailStone: { position: Location; direction: Location }, duration: number): Location {
         const x = hailStone.position.x + hailStone.direction.x * duration;
@@ -96,25 +117,7 @@ export namespace Y2023_Day24 {
         const duration = -position / velocity;
         return { duration, location: applyDuration(hailStone, duration) };
     }
-    function get3DParameters({
-        direction,
-        x0,
-        y0,
-    }: {
-        position: Location;
-        direction: Location;
-        x0: Location;
-        y0: Location;
-    }): Parameters3D | null {
-        const bDivider = -direction.y / direction.x + x0.y;
-        const b = (x0.z - y0.z + direction.z / direction.x) / bDivider;
-        const a = (b * direction.y - direction.z) / -direction.x;
-        const c = -a * y0.x - y0.z;
-
-        // TODO ADD A BACKUP FAILURE FOR WHEN b is null
-        return { a, b, c };
-    }
-    function get2DIntersection(hailStone: HailStone, otherHailStone: HailStone): Intersection2D | null {
+    function get2DIntersection(hailStone: BaseHailStone, otherHailStone: BaseHailStone): Intersection2D | null {
         const {
             parameters2d: { a: a1, b: b1 },
         } = hailStone;
@@ -127,8 +130,8 @@ export namespace Y2023_Day24 {
         if (divider === 0) return null;
         const x = (b2 - b1) / divider;
         const y = a1 * x + b1;
-        const durationA = (x - hailStone.position.x) / hailStone.direction.x;
-        const durationB = (x - otherHailStone.position.x) / otherHailStone.direction.x;
+        const durationA = smartRound((x - hailStone.position.x) / hailStone.direction.x);
+        const durationB = smartRound((x - otherHailStone.position.x) / otherHailStone.direction.x);
         return { x, y, durationA, durationB, a: hailStone, b: otherHailStone };
     }
     function getAllIntersections(hailStones: HailStone[]): Intersection2D[] {
@@ -156,11 +159,6 @@ export namespace Y2023_Day24 {
     }
     export function part1(lines: string[], min: number, max: number): number {
         const hailStones = lines.map(parseHailStone);
-        console.log(
-            'hailstone counts',
-            hailStones.filter((x) => typeof x.parameters2d?.a !== 'number').length,
-            hailStones.filter((x) => typeof x.parameters3d?.a !== 'number').length,
-        );
         const result = getAllIntersections(hailStones);
         const filtered = result.filter(({ x, y, durationA, durationB }) => {
             if (durationA < 0 || durationB < 0) return false;
@@ -168,23 +166,79 @@ export namespace Y2023_Day24 {
         });
         return filtered.length;
     }
+    function findRockPath(rock: Rock, hailStones: HailStone[]): Rock | null {
+        if (rock.hailStoneCollisions.length === hailStones.length) return rock;
+        if (rock.hailStoneCollisions.length > 2)
+            console.log(
+                rock.hailStoneCollisions.reduce(
+                    (t, hailStone) => t + ` ${hailStone.index}@${hailStone.duration}`,
+                    `${rock.direction.x},${rock.direction.y},${rock.direction.z}`,
+                ),
+            );
+        const currentTimeNs = rock.timeNs;
+        // const position = rock.position;
+        for (let i = 0; i < hailStones.length; i++) {
+            const hailStone = hailStones[i];
+            if (rock.hailStoneCollisions.some((x) => x.index === i)) continue;
+            const collission = getNextCollision(rock, hailStone, rock.timeNs);
+            if (collission === null) continue;
+            rock.timeNs = collission.duration;
+            // rock.position = collission.position;
+            rock.hailStoneCollisions.push({ ...hailStone, index: i, duration: collission.duration });
+
+            const result = findRockPath(rock, hailStones);
+            if (result !== null) return result;
+
+            // reset
+            rock.timeNs = currentTimeNs;
+            // rock.position = position;
+            rock.hailStoneCollisions.pop();
+        }
+        return null;
+    }
+    function tryAllRockPaths(hailStones: HailStone[]): Rock {
+        let counter = 1;
+        while (true) {
+            if (counter % 100 === 0) console.log('counter', counter);
+            console.log('counter', counter, hailStones.length);
+            for (let i = 0; i < hailStones.length; i++) {
+                const hailStone = hailStones[i];
+                const position = applyDuration(hailStone, 1);
+                for (let j = 0; j < hailStones.length; j++) {
+                    if (j === i) continue;
+                    const otherHailStone = hailStones[j];
+                    const nextPosition = applyDuration(otherHailStone, 1 + counter);
+                    const direction: Location = {
+                        x: (nextPosition.x - position.x) / counter,
+                        y: (nextPosition.y - position.y) / counter,
+                        z: (nextPosition.z - position.z) / counter,
+                    };
+                    const basePosition = applyDuration({ position, direction }, -1);
+                    const rock = findRockPath(
+                        {
+                            hailStoneCollisions: [
+                                { ...hailStone, index: i, duration: 1 },
+                                { ...otherHailStone, index: j, duration: 1 + counter },
+                            ],
+                            position: basePosition,
+                            direction,
+                            timeNs: 1 + counter,
+                            parameters2d: get2DParameters({ position: basePosition, direction }),
+                        },
+                        hailStones,
+                    );
+                    if (rock !== null) return rock;
+                }
+            }
+            counter++;
+        }
+    }
     export function part2(lines: string[]): number {
         const hailStones = lines.map(parseHailStone);
-        hailStones.forEach((hailStone) => {
-            const { a, b, c } = get3DParameters(hailStone);
-            console.log(JSON.stringify(hailStone));
-            console.log(JSON.stringify({ a, b, c }));
-            console.log();
-        });
-        // for (let i = 0; i < hailStones.length; i++) {
-        //     const hailStone = hailStones[i];
-        //     for (let j = i + 1; j < hailStones.length; j++) {
-        //         const otherHailStone = hailStones[j];
-        //         const intersection = get3DIntersection(hailStone, otherHailStone);
-        //         if (intersection) output.push(intersection);
-        //     }
-        // }
-        return 0;
+        const {
+            position: { x, y, z },
+        } = tryAllRockPaths(hailStones);
+        return x + y + z;
     }
 }
 
@@ -194,21 +248,6 @@ if (!module.parent) {
     const path = require('path');
 
     const main = async () => {
-        // UNIT TESTS
-        assert.equal(Y2023_Day24.smartRound(0.005500001), 0.0055);
-        assert.equal(Y2023_Day24.smartRound(1.000000001), 1);
-        assert.equal(Y2023_Day24.smartRound(2.8333333333333335), 2.8333333333333335);
-        // const exampleBaseHailStone1: Y2023_Day24.BaseHailStone = {
-        //     position: { x: 19, y: 13, z: 30 },
-        //     direction: { x: -2, y: 1, z: -2 },
-        // };
-        const exampleHailStone1 = Y2023_Day24.parseHailStone('19, 13, 30 @ -2,  1, -2');
-        assert.equal(exampleHailStone1.parameters3d.b, 85 / 30);
-        assert.equal(exampleHailStone1.parameters3d.c, -52.75);
-        // TODO VERIFY THE A, B, C
-        Y2023_Day24.applyFormulaDuration(exampleHailStone1, 1);
-        Y2023_Day24.applyFormulaDuration(exampleHailStone1, 2);
-
         const exampleLines = await FileEngine.readFileToLines(
             path.join(path.dirname(__filename), './data/y2023_day24.example'),
             false,
@@ -228,13 +267,13 @@ if (!module.parent) {
         console.log('part 1', part1Result, 'ms', Date.now() - startMs);
         assert.equal(part1Result, 28266);
         // part 2
-        assert.equal(Y2023_Day24.part2(exampleLines), 0, 'example part 2');
+        assert.equal(Y2023_Day24.part2(exampleLines), 47, 'example part 2');
 
-        // startMs = Date.now();
-        // console.log('part 2 start');
-        // const part2Result = Y2023_Day24.part2(lines);
-        // console.log('part 2', part2Result, 'ms', Date.now() - startMs);
-        // assert.equal(part2Result, 0);
+        startMs = Date.now();
+        console.log('part 2 start');
+        const part2Result = Y2023_Day24.part2(lines);
+        console.log('part 2', part2Result, 'ms', Date.now() - startMs);
+        assert.equal(part2Result, 0);
     };
 
     main().catch((err) => console.error(err));
