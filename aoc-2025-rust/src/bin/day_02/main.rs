5c687304e848605c8e2c806f5df0676b55a55e79
Hendrik Hamerlinck <hendrik.hamerlinck@hammernet.be>
2025-12-06 16:55:43 +0100

    2025 day 2 part 2

--- aoc-2025-rust/src/bin/day_02/main.rs
+++ aoc-2025-rust/src/bin/day_02/main.rs
@@ -11,6 +11,42 @@ mod part1 {
         left == right
     }
 
+    fn has_equal_pieces(s: &str, pieces: usize) -> bool {
+        let len = s.len();
+
+        if !len.is_multiple_of(pieces) {
+            return false;
+        }
+
+        let chunk_size = len / pieces;
+        if chunk_size == len {
+            return false;
+        }
+
+        let chunks: Vec<&[u8]> = s.as_bytes().chunks(chunk_size).collect();
+        let first = chunks[0];
+
+        chunks.iter().skip(1).all(|c| *c == first)
+    }
+
+    fn is_advanced_invalid_id(id: &str) -> bool {
+        let len: usize = id.len();
+
+        let mut pieces = 2;
+        loop {
+            if len / pieces < 1 {
+                return false;
+            }
+
+            let is_equal = has_equal_pieces(id, pieces);
+            if is_equal {
+                return true;
+            }
+
+            pieces += 1;
+        }
+    }
+
     impl ProductRange {
         pub fn new(line: &str) -> Self {
             let re = Regex::new(r"(\d+)-(\d+)").unwrap();
@@ -37,11 +73,32 @@ mod part1 {
             }
             counter
         }
+
+        pub fn count_advanced_invalid_ids(&self) -> u64 {
+            let mut counter = 0;
+            for id in self.min..=self.max {
+                // optimalization skip to next valid number
+                // uneven count of numbers should jump to even
+                let id_str = id.to_string();
+                if is_advanced_invalid_id(&id_str) {
+                    counter += id as u64;
+                }
+            }
+            counter
+        }
     }
 
     pub fn execute_part1(input: &str) -> u64 {
         let ranges: Vec<ProductRange> = input.split(",").map(ProductRange::new).collect();
         ranges.iter().map(ProductRange::count_invalid_ids).sum()
+    }
+
+    pub fn execute_part2(input: &str) -> u64 {
+        let ranges: Vec<ProductRange> = input.split(",").map(ProductRange::new).collect();
+        ranges
+            .iter()
+            .map(ProductRange::count_advanced_invalid_ids)
+            .sum()
     }
 }
 
@@ -57,6 +114,20 @@ fn part1_input() {
     let result = part1::execute_part1(day_input);
     eprintln!("{}", result);
     assert_eq!(result, 16793817782);
+}
+
+#[test]
+fn part2_example() {
+    let example_input = include_str!("./y2025_day02.example");
+    assert_eq!(part1::execute_part2(example_input), 4174379265);
+}
+
+#[test]
+fn part2_input() {
+    let day_input = include_str!("./y2025_day02.input");
+    let result = part1::execute_part2(day_input);
+    eprintln!("{}", result);
+    // assert_eq!(result, 16793817782);
 }
 
 fn main() {}
