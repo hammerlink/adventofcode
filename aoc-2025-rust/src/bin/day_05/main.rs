1b5e0cdb6f25549ca17890ec620d3e3956c333bf
Hendrik Hamerlinck <hendrik.hamerlinck@hammernet.be>
2025-12-09 21:56:35 +0100

    2025 day 5 part 2

--- aoc-2025-rust/src/bin/day_05/main.rs
+++ aoc-2025-rust/src/bin/day_05/main.rs
@@ -2,6 +2,10 @@ const EXAMPLE_INPUT: &str = include_str!("./y2025_day05.example");
 const INPUT: &str = include_str!("./y2025_day05.input");
 
 mod part1 {
+
+    use core::fmt;
+    use std::fmt::Display;
+
     use regex::Regex;
 
     struct Ingredient {
@@ -21,6 +25,40 @@ mod part1 {
                 min: captures[1].parse().unwrap(),
                 max: captures[2].parse().unwrap(),
             }
+        }
+
+        pub fn get_size(&self) -> u64 {
+            self.max - self.min + 1
+        }
+
+        pub fn overlaps(&self, other: &Range) -> bool {
+            (self.min >= other.min && self.min <= other.max)
+                || (other.min >= self.min && other.min <= self.max)
+                || (self.max >= other.min && self.max <= other.max)
+                || (other.max >= self.min && other.max <= self.max)
+        }
+
+        pub fn merge(&mut self, other: &Range) {
+            self.min = {
+                if self.min < other.min {
+                    self.min
+                } else {
+                    other.min
+                }
+            };
+            self.max = {
+                if self.max > other.max {
+                    self.max
+                } else {
+                    other.max
+                }
+            };
+        }
+    }
+
+    impl Display for Range {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "{}-{} ", self.min, self.max)
         }
     }
 
@@ -59,8 +97,38 @@ mod part1 {
             .count()
     }
 
+    fn get_overlap(ranges: &[Range]) -> Option<(usize, usize)> {
+        for (i, r1) in ranges.iter().enumerate() {
+            for (j, r2) in ranges.iter().enumerate().skip(i + 1) {
+                if r1.overlaps(r2) {
+                    return Some((i, j));
+                }
+            }
+        }
+        None
+    }
+
+    fn merge_ranges(ranges: Vec<Range>) -> Vec<Range> {
+        let mut updated_ranges: Vec<Range> = ranges;
+        updated_ranges.sort_by_key(|r| r.min);
+
+        while let Some((i, j)) = get_overlap(&updated_ranges) {
+            let other = updated_ranges.swap_remove(j);
+            updated_ranges.get_mut(i).unwrap().merge(&other);
+            updated_ranges.sort_by_key(|r| r.min);
+        }
+
+        updated_ranges
+    }
+
     pub fn execute_part2(input: &str) -> u64 {
-        0
+        let (ranges, _) = parse_input(input);
+        let ranges = merge_ranges(ranges);
+        // ranges.iter().for_each(|r| {
+        //     println!("{}", r);
+        // });
+        // too high -> not all are merged
+        ranges.iter().map(Range::get_size).sum()
     }
 }
 
@@ -82,14 +150,14 @@ fn part1_input() {
 fn part2_example() {
     let result = part1::execute_part2(EXAMPLE_INPUT);
     println!("{result}");
-    assert_eq!(result, 43);
+    assert_eq!(result, 14);
 }
 
 #[test]
 fn part2_input() {
     let result = part1::execute_part2(INPUT);
     println!("{result}");
-    assert_eq!(result, 8013);
+    assert!(result < 353244804666944);
 }
 
 fn main() {
