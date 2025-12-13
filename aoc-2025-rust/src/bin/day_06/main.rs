1c5899b916db2ff1d08f8955ca21af335bc3e930
Hendrik Hamerlinck <hendrik.hamerlinck@hammernet.be>
2025-12-13 11:03:28 +0100

    2025 day 6 part 2

--- aoc-2025-rust/src/bin/day_06/main.rs
+++ aoc-2025-rust/src/bin/day_06/main.rs
@@ -2,7 +2,11 @@ const EXAMPLE_INPUT: &str = include_str!("./y2025_day06.example");
 const INPUT: &str = include_str!("./y2025_day06.input");
 
 mod part1 {
-    #[derive(PartialEq)]
+    struct Range {
+        start_i: usize,
+        end_i: usize,
+    }
+    #[derive(PartialEq, Debug)]
     enum Operator {
         Plus,
         Multiply,
@@ -18,8 +22,9 @@ mod part1 {
     }
 
     struct Operation {
-        numbers: Vec<u64>,
+        numbers: Vec<u128>,
         operator: Operator,
+        range: Range,
     }
 
     impl Operation {
@@ -27,9 +32,13 @@ mod part1 {
             Operation {
                 numbers: vec![],
                 operator,
+                range: Range {
+                    start_i: 0,
+                    end_i: 0,
+                },
             }
         }
-        fn execute_calculation(&self) -> u64 {
+        fn execute_calculation(&self) -> u128 {
             self.numbers
                 .clone()
                 .into_iter()
@@ -66,13 +75,50 @@ mod part1 {
         operations
     }
 
-    pub fn execute_part1(input: &str) -> u64 {
+    pub fn execute_part1(input: &str) -> u128 {
         let operations = parse_input(input);
         operations.iter().map(Operation::execute_calculation).sum()
     }
 
-    pub fn execute_part2(input: &str) -> u64 {
-        0
+    fn parse_input2(input: &str) -> Vec<Operation> {
+        let mut raw_pieces: Vec<&str> = input.lines().collect();
+        let operator_line = raw_pieces.pop().unwrap().to_string();
+
+        let mut operations = vec![];
+        let mut current_operation: Operation = Operation::new(Operator::Plus);
+        for (i, v) in operator_line.chars().enumerate() {
+            let is_space = v == ' ';
+            if !is_space {
+                if i > 1 {
+                    current_operation.range.end_i = i - 2;
+                    operations.push(current_operation);
+                }
+                current_operation = Operation::new(Operator::new(&v.to_string()));
+                current_operation.range.start_i = i;
+            }
+        }
+        current_operation.range.end_i = operator_line.len() - 1;
+        operations.push(current_operation);
+
+        for operation in operations.iter_mut() {
+            for i in operation.range.start_i..=operation.range.end_i {
+                let mut num_string: String = "".to_string();
+                for piece in raw_pieces.iter() {
+                    let char = &piece[i..=i];
+                    if char != " " {
+                        num_string += char;
+                    }
+                }
+                operation.numbers.push(num_string.parse().unwrap());
+            }
+        }
+
+        operations
+    }
+
+    pub fn execute_part2(input: &str) -> u128 {
+        let operations = parse_input2(input);
+        operations.iter().map(Operation::execute_calculation).sum()
     }
 }
 
@@ -87,12 +133,14 @@ fn part1_example() {
 fn part1_input() {
     let result = part1::execute_part1(INPUT);
     println!("{result}");
+    assert_eq!(result, 4076006202939);
 }
 
 #[test]
 fn part2_example() {
     let result = part1::execute_part2(EXAMPLE_INPUT);
     println!("{result}");
+    assert_eq!(result, 3263827);
 }
 
 #[test]
