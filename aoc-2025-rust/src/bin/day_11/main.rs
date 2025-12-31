f996f24d4aa5b8daec340e721fb416c721814d7c
Hendrik Hamerlinck <hendrik.hamerlinck@hammernet.be>
2025-12-31 13:06:32 +0100

    2025 day 11 part 2

--- aoc-2025-rust/src/bin/day_11/main.rs
+++ aoc-2025-rust/src/bin/day_11/main.rs
@@ -1,16 +1,27 @@
 #[allow(unused)]
 const EXAMPLE_INPUT: &str = include_str!("./y2025_day11.example");
+const EXAMPLE_INPUT2: &str = include_str!("./y2025_day11.example2");
 const INPUT: &str = include_str!("./y2025_day11.input");
 
 mod part1 {
-    use std::collections::{HashMap, HashSet};
+    use std::{
+        cell::RefCell,
+        collections::{HashMap, HashSet},
+    };
 
     const YOU: &str = "you";
     const OUT: &str = "out";
+    const SVR: &str = "svr";
+    const FFT: &str = "fft";
+    const DAC: &str = "dac";
 
     struct ServerRack {
         id: String,
         connections: HashSet<String>, // TODO not all connections are here yet
+        has_none_stored_count: RefCell<Option<usize>>,
+        has_fft_stored_count: RefCell<Option<usize>>,
+        has_dac_stored_count: RefCell<Option<usize>>,
+        has_both_stored_count: RefCell<Option<usize>>,
     }
 
     impl ServerRack {
@@ -25,6 +36,10 @@ mod part1 {
             ServerRack {
                 id: id.to_string(),
                 connections,
+                has_none_stored_count: None.into(),
+                has_fft_stored_count: None.into(),
+                has_dac_stored_count: None.into(),
+                has_both_stored_count: None.into(),
             }
         }
         fn count_out_distances(&self, map: &HashMap<String, ServerRack>) -> usize {
@@ -38,6 +53,63 @@ mod part1 {
                     count += 1;
                 } else if let Some(connection) = map.get(connection_id) {
                     count += connection.count_out_distances(map);
+                }
+            }
+
+            count
+        }
+
+        fn get_stored_count(&self, has_fft: bool, has_dac: bool) -> Option<usize> {
+            if !has_fft && !has_dac {
+                *self.has_none_stored_count.borrow()
+            } else if has_dac && !has_fft {
+                *self.has_dac_stored_count.borrow()
+            } else if has_fft && !has_dac {
+                *self.has_fft_stored_count.borrow()
+            } else {
+                *self.has_both_stored_count.borrow()
+            }
+        }
+
+        fn store_count(&self, has_fft: bool, has_dac: bool, value: usize) {
+            if !has_fft && !has_dac {
+                *self.has_none_stored_count.borrow_mut() = Some(value);
+            } else if has_dac && !has_fft {
+                *self.has_dac_stored_count.borrow_mut() = Some(value);
+            } else if has_fft && !has_dac {
+                *self.has_fft_stored_count.borrow_mut() = Some(value);
+            } else {
+                *self.has_both_stored_count.borrow_mut() = Some(value);
+            }
+        }
+
+        fn count_out_distances_with_tracking(
+            &self,
+            map: &HashMap<String, ServerRack>,
+            has_fft: bool,
+            has_dac: bool,
+        ) -> usize {
+            if self.id == OUT {
+                panic!("should not get here");
+            }
+            let mut count = 0;
+
+            for connection_id in self.connections.iter() {
+                if connection_id == OUT {
+                    if has_fft && has_dac {
+                        count += 1;
+                    }
+                } else if let Some(connection) = map.get(connection_id) {
+                    let has_fft = has_fft || connection_id == FFT;
+                    let has_dac = has_dac || connection_id == DAC;
+                    if let Some(cached_count) = connection.get_stored_count(has_fft, has_dac) {
+                        count += cached_count;
+                    } else {
+                        let connection_count =
+                            connection.count_out_distances_with_tracking(map, has_fft, has_dac);
+                        connection.store_count(has_fft, has_dac, connection_count);
+                        count += connection_count;
+                    }
                 }
             }
 
@@ -57,7 +129,12 @@ mod part1 {
 
     #[allow(unused)]
     pub fn execute_part2(input: &str) -> usize {
-        0
+        let server_racks = input.lines().map(ServerRack::new);
+        let server_rack_map: HashMap<String, ServerRack> = server_racks
+            .map(|server_rack| (server_rack.id.clone(), server_rack))
+            .collect();
+        let start = server_rack_map.get(SVR).expect("srv to be found");
+        start.count_out_distances_with_tracking(&server_rack_map, false, false)
     }
 }
 
@@ -77,8 +154,9 @@ fn part1_input() {
 
 #[test]
 fn part2_example() {
-    let result = part1::execute_part2(EXAMPLE_INPUT);
+    let result = part1::execute_part2(EXAMPLE_INPUT2);
     println!("{result}");
+    assert_eq!(result, 2);
 }
 #[test]
 fn part2_input() {
@@ -87,8 +165,9 @@ fn part2_input() {
 }
 
 fn main() {
-    // part1::execute_part1(EXAMPLE_INPUT);
-    // part1::execute_part1(INPUT);
+    part1::execute_part1(EXAMPLE_INPUT);
+    part1::execute_part1(INPUT);
+    part1::execute_part2(EXAMPLE_INPUT2);
     let result = part1::execute_part2(INPUT);
     println!("result {}", result);
 }
